551cf542d497cc0f5e08a79a87421773f751b838
niyonkurutresor <niyonkurutresor17@gmail.com>
2022-08-16 11:07:03 -0700

    added cloudinary storage

--- django_demo/settings.py
+++ django_demo/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 import sys
 import django_heroku
+import cloudinary_storage
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -43,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     'crispy_forms',
     'movies',
     'users',
@@ -99,7 +102,7 @@ DATABASES = {
        'PORT': '5432',
    }
 }
-
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 import dj_database_url
 db_from_env=dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
@@ -154,4 +157,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': 'kist',
+    'API_KEY': '168412473156841',
+    'API_SECRET': '5qJHJLGBavBnTmMTG7fy19GYWq8'
+}
 
